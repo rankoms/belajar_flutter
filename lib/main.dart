74d132fccaff963370c1dbec47f71903d4eda9b9
rankoms <rankom202@gmail.com>
2022-01-06 10:31:41 +0700

    ERICO 06 statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 2;
+  void tekanTombol() {
+    setState(() {
+      number++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Latihan Container dan Widget'),
-        ),
-        body: Container(
-          color: Colors.red,
-          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-          margin: EdgeInsets.only(bottom: 20),
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [Colors.amber, Colors.blue])),
-            margin: EdgeInsets.all(10),
+        appBar: AppBar(title: Text('Ini adalah statefull widget')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                number.toString(),
+                style: TextStyle(fontSize: 10 + number.toDouble()),
+              ),
+              ElevatedButton(
+                onPressed: tekanTombol,
+                child: Text('Tekan Tombol'),
+              )
+            ],
           ),
         ),
       ),
