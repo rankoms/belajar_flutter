4a4b95e2527d47e68f183f5b1bcb25fab13a67ed
rankoms <rankom202@gmail.com>
2022-01-07 10:52:00 +0700

    latihan animated container

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,37 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Random random = Random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Latihan Text Style'),
+          title: Text('Halo'),
         ),
         body: Center(
-          child: Text(
-            'Ini adalh Test',
-            style: TextStyle(
-                fontFamily: "TheNautigal",
-                fontSize: 30,
-                decoration: TextDecoration.underline,
-                decorationStyle: TextDecorationStyle.dashed),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {});
+            },
+            child: AnimatedContainer(
+              color: Color.fromARGB(255, random.nextInt(256),
+                  random.nextInt(256), random.nextInt(256)),
+              duration: Duration(seconds: 1),
+              width: 50.0 + random.nextInt(101),
+              height: 50.0 + random.nextInt(101),
+            ),
           ),
         ),
       ),
