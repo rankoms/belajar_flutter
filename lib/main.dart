75dd62f365ece8e64ee302208e21f4bb5a27b9a9
rankoms <rankom202@gmail.com>
2022-01-07 10:40:42 +0700

    08 text style

--- lib/main.dart
+++ lib/main.dart
@@ -2,52 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int counter = 1;
-  List<Widget> widgets = [];
-  // _MyAppState() {
-  //   for (int i = 0; i < 15; i++) {
-  //     widgets.add(Text(
-  //       'Data Ke ' + i.toString(),
-  //       style: TextStyle(fontSize: 24),
-  //     ));
-  //   }
-  // }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello List VIew'),
+          title: Text('Latihan Text Style'),
         ),
-        body: ListView(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        widgets.add(Text('Data ke -' + counter.toString()));
-                        counter++;
-                      });
-                    },
-                    child: Text('Tambah Data')),
-                ElevatedButton(onPressed: null, child: Text('Hapus Data'))
-              ],
-            ),
-            Column(
-              children: widgets,
-              crossAxisAlignment: CrossAxisAlignment.start,
-            )
-          ],
+        body: Center(
+          child: Text(
+            'Ini adalh Test',
+            style: TextStyle(
+                fontFamily: "TheNautigal",
+                fontSize: 30,
+                decoration: TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.dashed),
+          ),
         ),
       ),
     );
