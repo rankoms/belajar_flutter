a1b60e88bd847e0b579a06ded0fd657f064f834c
rankoms <rankom202@gmail.com>
2022-01-10 10:26:48 +0700

    add image widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,126 +10,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Stack'),
+          title: Text("Ini  Image 2"),
         ),
-        body: Stack(
-          children: [
-            Column(
-              children: [
-                Flexible(
-                  child: Row(
-                    children: [
-                      Flexible(
-                        child: Container(
-                          color: Colors.white,
-                        ),
-                        flex: 1,
-                      ),
-                      Flexible(
-                        child: Container(
-                          color: Colors.black12,
-                        ),
-                        flex: 1,
-                      )
-                    ],
-                  ),
-                  flex: 1,
-                ),
-                Flexible(
-                  child: Row(children: [
-                    Flexible(
-                      child: Container(
-                        color: Colors.black12,
-                      ),
-                      flex: 1,
-                    ),
-                    Flexible(
-                      child: Container(
-                        color: Colors.white,
-                      ),
-                      flex: 1,
-                    )
-                  ]),
-                  flex: 1,
-                )
-              ],
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            padding: EdgeInsets.all(5),
+            color: Colors.red,
+            child: Image(
+              image: AssetImage('images/100.png'),
+              // fit: BoxFit.cover,
+              // repeat: ImageRepeat.repeat,
             ),
-            ListView(
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.all(10),
-                      child: Text(
-                        'Ini adalah text yang ada di tengan lapisan stack',
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.all(10),
-                      child: Text(
-                        'Ini adalah text yang ada di tengan lapisan stack',
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.all(10),
-                      child: Text(
-                        'Ini adalah text yang ada di tengan lapisan stack',
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.all(10),
-                      child: Text(
-                        'Ini adalah text yang ada di tengan lapisan stack',
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.all(10),
-                      child: Text(
-                        'Ini adalah text yang ada di tengan lapisan stack',
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.all(10),
-                      child: Text(
-                        'Ini adalah text yang ada di tengan lapisan stack',
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.all(10),
-                      child: Text(
-                        'Ini adalah text yang ada di tengan lapisan stack',
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.all(10),
-                      child: Text(
-                        'Ini adalah text yang ada di tengan lapisan stack',
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.all(10),
-                      child: Text(
-                        'Ini adalah text yang ada di tengan lapisan stack',
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-            Align(
-                alignment: Alignment(0, 1),
-                child:
-                    ElevatedButton(onPressed: null, child: Text('My Button')))
-          ],
+          ),
         ),
       ),
     );
