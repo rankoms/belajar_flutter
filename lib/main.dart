c34cf6f9307b4982dea0d58a04b64d71c1452f79
rankoms <rankom202@gmail.com>
2022-01-11 14:41:41 +0700

    membuat media query portrait dan landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main_page.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: MainPage(),
+    );
   }
 }
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -7,19 +7,36 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Main Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Go To second Page'),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return SecondPage();
-            }));
-          },
+        appBar: AppBar(
+          title: Text('Latihan Media Query'),
         ),
+        body: (MediaQuery.of(context).orientation == Orientation.portrait)
+            ? Column(children: methodContainer)
+            : Row(children: methodContainer));
+  }
+
+  List<Widget> get methodContainer {
+    return [
+      Container(
+        color: Colors.red,
+        width: 100,
+        height: 100,
       ),
-    );
+      Container(
+        color: Colors.green,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.blue,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.orange,
+        width: 100,
+        height: 100,
+      ),
+    ];
   }
 }
