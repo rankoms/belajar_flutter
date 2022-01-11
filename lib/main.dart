734b749cb8b483bd42b077b96008e7338888e7e5
rankoms <rankom202@gmail.com>
2022-01-11 14:59:06 +0700

    membuat button menggunakan inkwell

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Latihan button'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text('Press Me')),
+            Material(
+              elevation: 5,
+              borderRadius: BorderRadius.circular(20),
+              child: Container(
+                width: 150,
+                height: 40,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                        colors: [Colors.purple, Colors.pink],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter)),
+                child: Material(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(20),
+                  child: InkWell(
+                    splashColor: Colors.amber,
+                    borderRadius: BorderRadius.circular(20),
+                    onTap: () {},
+                    child: Center(
+                        child: Text(
+                      'My Button',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w600),
+                    )),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
