0a2f7973148b320ba12f83a413f80205dc369d2c
rankoms <rankom202@gmail.com>
2022-01-13 10:36:18 +0700

    membuat ketupat

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_application_1/main_page.dart';
+import 'package:flutter_application_1/colorful_button.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,45 +8,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    TabBar myTabBar = TabBar(
-      indicatorColor: Colors.red,
-      indicator: BoxDecoration(
-          color: Colors.green,
-          border: Border(bottom: BorderSide(color: Colors.purple, width: 5))),
-      tabs: [
-        Tab(
-          icon: Icon(Icons.comment),
-          text: "Comment",
-        ),
-        Tab(
-          child: Image(
-            image: AssetImage('images/facebook.png'),
-          ),
-        ),
-      ],
-    );
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: DefaultTabController(
-          length: 2,
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text('Contoh Tab Bar'),
-              bottom: PreferredSize(
-                  preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
-                  child: Container(color: Colors.amber, child: myTabBar)),
-            ),
-            body: TabBarView(
-              children: [
-                Center(
-                  child: Text("Tab 1"),
-                ),
-                Center(
-                  child: Text("Tab 1"),
-                ),
-              ],
-            ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('asdaf'),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ColorfulButton(Colors.yellow, Colors.blue, Icons.adb),
+              ColorfulButton(
+                  Colors.yellow, Colors.blue, Icons.add_alarm_rounded),
+              ColorfulButton(Colors.yellow, Colors.blue, Icons.facebook),
+              ColorfulButton(
+                  Colors.yellow, Colors.blue, Icons.add_a_photo_outlined),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
