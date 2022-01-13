29995da4f8af6457474d9e0941a89e5574391c4f
rankoms <rankom202@gmail.com>
2022-01-13 13:37:55 +0700

    membuat qrcode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -9,6 +12,13 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text('Hello world'),
+        ),
+        body: Container(
+          child: QrImage(
+            data: "1234567890",
+            version: QrVersions.auto,
+            size: 200.0,
+          ),
         ),
       ),
     );
