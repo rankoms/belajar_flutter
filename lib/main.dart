ab8f2f23a12288044ba5cd1ad09b600bad4304c6
rankoms <rankom202@gmail.com>
2022-01-17 09:11:56 +0700

    membuat clippath

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('CUstom'),
+      ),
+      body: Center(
+        child: ClipPath(
+          clipper: MyClipper(),
+          child: Image(
+            width: 300,
+            image: AssetImage('images/pemandangan.jpg'),
+          ),
+        ),
+      ),
+    ));
   }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width / 2, size.height * 0.75, size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
